440107295fbb65fb4bdd43eaa8bf98eb040f2494
Kalvis Kincis <kalvis.kincis@r6vsk.lv>
2023-09-28 17:04:46 +0300

    2023-09-28 (fstring, dažas metodes simbolu virkņu apstrādei)

---
+++ 2023-2024/G1/2023-09-28/02-einsteins.py
@@ -0,0 +1,4 @@
+m = int(input("Uzraksti masu kg"))
+c = 300000000
+e_jouli = m*c**2
+print(f"E={e_jouli} J")
---
+++ 2023-2024/G2/2023-09-28/02-print-mainigie-input-fstring.py
@@ -0,0 +1,15 @@
+# print parāda tekstu, skaitļus utt. ekrānā
+vards = input("Raksti savu vārdu").strip()
+print("Labdien,", vards, "!")
+print(f"Labdien, {vards}!")
+print(100**3)
+
+
+# ja jāparāda teksts un, piemēram, izteiksme, atdala ar komatu
+print("100**3=", 100**3)
+
+# veidojam mainīgos
+# a = int(input("Raksti skaitli a"))
+# b = int(input("Raksti skaitli b"))
+# print(a+b)
+
