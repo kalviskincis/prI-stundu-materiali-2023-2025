552cb78df51ff7c87d70401163ae5661c922e934
Kalvis Kincis <kalvis.kincis@r6vsk.lv>
2023-12-05 11:14:48 +0200

    tkreklu uzdevums

--- 2023-2024/G2/2023-11-16/06-tkrekli.py
+++ 2023-2024/G2/2023-11-16/06-tkrekli.py
@@ -1,10 +1,25 @@
 def pasuti_tkreklus(skaits, apdruka, piegade):
-    # jūsu kods
+    if apdruka == "TEKSTS":
+        summa = skaits * 5
+    elif apdruka == "ZIME":
+        summa = skaits * 7
+    elif apdruka == "FOTO":
+        summa = skaits * 20
+    else:
+        print("Kļūda")
 
-    
-    
+    if piegade == True:
+        if summa >= 50:
+            piegades_maksa = 0
+        else:
+            piegades_maksa = 15
+        
+        summa = summa + piegades_maksa
+
+    if summa > 100:
+        summa = summa * 0.95
+        # summa = summa - summa * 0.05
     
     return summa 
 
-
-print(pasuti_tkreklus(10, "ZIME", True))
+print(pasuti_tkreklus(100, "ZIME", False))
