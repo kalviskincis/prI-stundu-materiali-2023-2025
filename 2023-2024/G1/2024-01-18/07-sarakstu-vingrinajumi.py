01f41c0539f7394fd0eb4eabb8bc15f8936f732b
Kalvis Kincis <kalvis.kincis@r6vsk.lv>
2024-01-23 11:12:43 +0200

    2024-01-18 sarakstu vingrinājumi

---
+++ 2023-2024/G1/2024-01-18/07-sarakstu-vingrinajumi.py
@@ -0,0 +1,20 @@
+bakterijas = ['Bacteria', 'Protozoa', 'Chromista', 'Plantae', 'Fungi', 'Animalia']
+print(bakterijas[0])
+print(bakterijas[-1])
+print(bakterijas[0:3])
+print(bakterijas[2:5])
+print(bakterijas[4:6])
+
+tiksanas =  ['9:00', '10:30', '14:00', '15:00', '15:30']
+tiksanas.append('16:30')
+tiksanas.append('18:30')
+tiksanas.extend(['19:00', '20:00'])
+
+pilsetas = ['Jelgava', 'Ķekava', 'Lubāna', 'Mazsalaca', 'Sabile', 'Ainaži', 'Baloži', 'Cesvaine', 'Dobele', 'Gulbene']
+pilsetas.append('Iecava')
+pilsetas.insert(2, 'Vecpiebalga')
+pilsetas.sort()
+print(pilsetas)
+pilsetas.remove('Iecava')
+pilsetas.pop()
+
