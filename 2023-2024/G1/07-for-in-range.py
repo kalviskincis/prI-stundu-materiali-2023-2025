382759cd71a6f0c33f00293750af00899330317e
Kalvis Kincis <kalvis.kincis@r6vsk.lv>
2024-02-08 15:22:04 +0200

    2024-02-08 for in range

---
+++ 2023-2024/G1/07-for-in-range.py
@@ -0,0 +1,13 @@
+#skaitlis = 1
+#while skaitlis <= 10:
+    # print(skaitlis)
+#    print(f"{skaitlis}. Es vairs nekavēšu programmēšanas stundu sākumus.")
+#    skaitlis += 1
+
+#for i in range(1, 11):
+#    print(i)
+
+summa = 0
+for sk in range(1, 101):
+    summa += sk
+print(summa)
---
+++ 2023-2024/G2/2024-02-08/07-for-in-range.py
@@ -0,0 +1,11 @@
+"""
+skaits = 0
+while skaits <= 10:
+    # print("Simpsons raksta uz tāfeles.")
+    print(skaits)
+    skaits += 1
+"""
+
+for skaitlis in range(10):
+    # print(skaitlis)
+    print("Simpsons raksta uz tāfeles.")
