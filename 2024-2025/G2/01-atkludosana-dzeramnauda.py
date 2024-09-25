cecc1c54f5b2235a771f01bc1a500f2a06a40e4f
Kalvis Kincis <kalvis.kincis@r6vsk.lv>
2024-09-25 14:01:03 +0300

    Update 01-atkludosana-dzeramnauda.py

--- 2024-2025/G2/01-atkludosana-dzeramnauda.py
+++ 2024-2025/G2/01-atkludosana-dzeramnauda.py
@@ -2,10 +2,10 @@ def main():
     eiro = cena_uz_float(input("Cik maksāja maltīte? "))
     procenti = procenti_uz_float(input("Cik procentus vēlies atstāt dzeramnaudā? "))
     atstat = eiro * procenti
-    print(f"Atstāj ${atstat:.2f}")
+    print(f"Atstāj €{atstat:.2f}")
 
 def cena_uz_float(e):
-    eiro = float(e.strip("$"))
+    eiro = float(e.strip("€"))
     return eiro
 
 def procenti_uz_float(p):
