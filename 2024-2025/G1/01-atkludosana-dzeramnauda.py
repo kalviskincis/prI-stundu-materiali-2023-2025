62d4657e8a60fad153f0f748185aed621030bb1a
Kalvis Kincis <kalvis.kincis@r6vsk.lv>
2024-09-25 12:46:49 +0000

    stundas 2024-09-25

--- 2024-2025/G1/01-atkludosana-dzeramnauda.py
+++ 2024-2025/G1/01-atkludosana-dzeramnauda.py
@@ -1,15 +1,24 @@
 def main():
     eiro = cena_uz_float(input("Cik maksāja maltīte? "))
     procenti = procenti_uz_float(input("Cik procentus vēlies atstāt dzeramnaudā? "))
-    atstat = eiro * procenti
-    print(f"Atstāj ${atstat:.2f}")
+    try:
+        atstat = eiro * procenti
+        print(f"Atstāj €{atstat:.2f}")
+    except TypeError:
+        print("Nevarēju veikt aprēķinus. Nepareizi dati.")
 
 def cena_uz_float(e):
-    eiro = float(e.strip("$"))
-    return eiro
+    try:
+        eiro = float(e.strip("€"))
+        return eiro
+    except ValueError:
+        print("Netika ievadīta korekta vērtība.")
 
 def procenti_uz_float(p):
-    procenti = float(p.strip("%"))/100
-    return procenti
+    try:
+        procenti = float(p.strip("%"))/100
+        return procenti
+    except ValueError:
+        print("Netika ievadīta korekta vērtība.")
 
 main()
--- 2024-2025/G2/01-atkludosana-dzeramnauda.py
+++ 2024-2025/G2/01-atkludosana-dzeramnauda.py
@@ -1,15 +1,24 @@
 def main():
     eiro = cena_uz_float(input("Cik maksāja maltīte? "))
     procenti = procenti_uz_float(input("Cik procentus vēlies atstāt dzeramnaudā? "))
-    atstat = eiro * procenti
-    print(f"Atstāj ${atstat:.2f}")
+    try:
+        atstat = eiro * procenti
+        print(f"Atstāj €{atstat:.2f}")
+    except TypeError:
+        print("Nebija iespējams aprēķināt.")
 
 def cena_uz_float(e):
-    eiro = float(e.strip("$"))
-    return eiro
+    try:
+        eiro = float(e.strip("€"))
+        return eiro
+    except ValueError:
+        print("Nepareizi ievadīta vērtība.")
 
 def procenti_uz_float(p):
-    procenti = float(p.strip("%"))/100
-    return procenti
+    try:
+        procenti = float(p.strip("%"))/100
+        return procenti
+    except ValueError:
+        print("Nepareizi ievadīta vērtība.")
 
 main()
